061804e2ec997cd6d32b540810857081a7559d37
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-09-03 13:44:05 +0200

    extra assignments

---
+++ CoffeeShopConsoleAppNet60/OrderLine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopConsoleAppNet60
+{
+    public class OrderLine
+    {
+
+        public OrderLine(int amount, Coffee item)
+        {
+            if (amount > 1)
+            {
+                throw new ArgumentOutOfRangeException("Antal kan ikke være 0 eller mindre");
+            }
+            Amount = amount;
+            Item = item;
+        }
+        public int Amount { get; set; }
+
+        public Coffee Item { get; set; }
+
+        public int PriceWithDiscount()
+        {
+            return Item.PriceWithDiscount() * Amount;
+        }
+        public int Price()
+        {
+            return Item.Price() * Amount;
+        }
+        public int TotalDiscount()
+        {
+            return Item.Price() - Item.PriceWithDiscount();
+        }
+    }
+}
